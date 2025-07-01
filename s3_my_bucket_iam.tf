0459556f4c936dbcf824c98de54dec79cda2684e
Ondrej Sika <ondrej@ondrejsika.com>
2025-07-01 11:48:51 +0200

    feat: Add write only policy for my-bucket

--- s3_my_bucket_iam.tf
+++ s3_my_bucket_iam.tf
@@ -14,3 +14,19 @@ resource "aws_iam_policy" "my_bucket_full_access" {
   description = "Policy for full access to ${aws_s3_bucket.my_bucket.bucket}"
   policy      = data.aws_iam_policy_document.my_bucket_full_access.json
 }
+
+data "aws_iam_policy_document" "my_bucket_write_only" {
+  statement {
+    actions = [
+      "s3:PutObject",
+      "s3:PutObjectAcl"
+    ]
+    resources = ["${aws_s3_bucket.my_bucket.arn}/*"]
+  }
+}
+
+resource "aws_iam_policy" "my_bucket_write_only" {
+  name        = "my-bucket-write-only-policy"
+  description = "Policy for write-only access to ${aws_s3_bucket.my_bucket.bucket}"
+  policy      = data.aws_iam_policy_document.my_bucket_write_only.json
+}
