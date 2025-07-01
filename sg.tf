48ccc0dc7d9608ebf692a553f7c43e670b961e41
Ondrej Sika <ondrej@ondrejsika.com>
2025-07-01 14:51:28 +0200

    feat: create sg

---
+++ sg.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "allow_22_80" {
+  name   = "allow-22-80"
+  vpc_id = module.my_vpc.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
